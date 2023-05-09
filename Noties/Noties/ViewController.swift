114a7c33dc8a2f9b8169311902003f0b453f9693
NesquikZhenya <nesquik.zhenya@gmail.com>
2023-05-09 10:47:15 +0400

    Project setup

---
+++ Noties/Noties/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Noties
+//
+//  Created by Евгений Михневич on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
